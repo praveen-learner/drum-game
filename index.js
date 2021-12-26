62f3327e15d0bee0df8f14d6560e0c5657b2df6a
Praveen Kumar <75360904+praveen-learner@users.noreply.github.com>
2021-12-26 10:38:38 +0530

    drum game

---
+++ index.js
@@ -0,0 +1,84 @@
+
+
+
+var numberofbuttons = document.querySelectorAll(".drum").length;
+
+
+for (var i=0; i<numberofbuttons; i++) {
+    
+    document.querySelectorAll('.drum')[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+
+
+    }
+        
+    );
+};
+
+
+
+
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+
+});
+
+
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+           var tom1= new Audio("sounds/tom-1.mp3");
+           tom1.play();
+
+            break;
+
+        case "a":
+            var tom2= new Audio("sounds/tom-2.mp3");
+            tom2.play();
+     
+            break;
+        case "s":
+            var tom3= new Audio("sounds/tom-3.mp3");
+            tom3.play();
+         
+            break;
+        
+        case "d":
+            var tom4= new Audio("sounds/tom-4.mp3");
+            tom4.play();
+     
+            break;
+
+        case "j":
+            var crash= new Audio("sounds/crash.mp3");
+            crash.play();
+         
+            break;
+
+        case "k":
+            var kick= new Audio("sounds/kick-bass.mp3");
+            kick.play();
+             
+            break;
+
+        case "l":
+            var snare= new Audio("sounds/snare.mp3");
+            snare.play();
+                 
+            break;
+    
+        default:
+            break;
+
+}};
+
+
+
+
+
+
+
+
